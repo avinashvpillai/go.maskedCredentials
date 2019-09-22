84f69497d250cd64b850c6a67f8c19706c0c0202
Avinash V Pillai <37207365+avinashvpillai@users.noreply.github.com>
2019-09-22 16:14:19 +0530

    Add files via upload

--- build.cmd
+++ build.cmd
@@ -14,6 +14,22 @@ set GOOS=darvin
 set GOARCH=amd64
 go build -o executable_mac_amd64.dgm .
 
+set GOOS=darvin
+set GOARCH=arm64
+go build -o executable_mac_arm64.dgm .
+
+set GOOS=darvin
+set GOARCH=arm
+go build -o executable_mac_arm.dgm .
+
+set GOOS=darvin
+set GOARCH=386
+go build -o executable_mac_386.dgm .
+
 set GOOS=windows
 set GOARCH=amd64
 go build -o executable_win_amd64.exe .
+
+set GOOS=windows
+set GOARCH=386
+go build -o executable_win_386.exe .
