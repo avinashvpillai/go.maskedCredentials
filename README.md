765363da398d0393639ba8dc40cb40bb3794b900
Avinash V Pillai <37207365+avinashvpillai@users.noreply.github.com>
2019-09-22 17:18:28 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ accepting username &amp; masked passwork from cmd - GO language
 go build -o maskedUsernamePassword_0.0.1.exe .
 OR
 go build -ldflags "-X main.version=v0.0.1" -o maskedUsernamePassword_0.0.1.exe .
+
+* worth refer : https://github.com/karalabe/xgo
--- build.cmd
+++ build.cmd
@@ -1,35 +1,36 @@
 set GOOS=linux 
 set GOARCH=amd64 
-go build -o executable_linux_amdx64 .
+go build -ldflags "-X main.version=v0.0.1" -o executable_linux_amdx64 .
 
 set GOOS=linux
 set GOARCH=386
-go build -o executable_linux_x86 .
+go build -ldflags "-X main.version=v0.0.1" -o executable_linux_x86 .
 
 set GOOS=linux
 set GOARCH=arm64
-go build -o executable_linux_arm64 .
+go build -ldflags "-X main.version=v0.0.1" -o executable_linux_arm64 .
 
 set GOOS=darvin
 set GOARCH=amd64
-go build -o executable_mac_amd64.dgm .
+go build -ldflags "-X main.version=v0.0.1" -o executable_mac_amd64.dgm .
 
 set GOOS=darvin
 set GOARCH=arm64
-go build -o executable_mac_arm64.dgm .
+go build -ldflags "-X main.version=v0.0.1" -o executable_mac_arm64.dgm .
 
 set GOOS=darvin
 set GOARCH=arm
-go build -o executable_mac_arm.dgm .
+go build -ldflags "-X main.version=v0.0.1" -o executable_mac_arm.dgm .
 
 set GOOS=darvin
 set GOARCH=386
-go build -o executable_mac_386.dgm .
-
-set GOOS=windows
-set GOARCH=amd64
-go build -o executable_win_amd64.exe .
+go build -ldflags "-X main.version=v0.0.1" -o executable_mac_386.dgm .
 
 set GOOS=windows
 set GOARCH=386
-go build -o executable_win_386.exe .
+go build -ldflags "-X main.version=v0.0.1" -o auth-v0.0.1-win-386.exe .
+
+set GOOS=windows
+set GOARCH=amd64
+go build -ldflags "-X main.version=v0.0.1" -o auth-v0.0.1-win_amd64.exe .
+
